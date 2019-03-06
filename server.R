bac04cfb43f08f6565654c0c7e8225e44578745d
jinc132 <jinc132@u.washington.edu>
2019-03-06 11:48:29 -0800

    add shiny web

---
+++ server.R
@@ -0,0 +1,10 @@
+library(shiny)
+library(plotly)
+library(dplyr)
+
+# supress warning messages
+suppressWarnings(warnings)
+
+shinyServer(function(input, output) {
+  
+})
---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(plotly)
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("United States Exports"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("bins",
+                   "Number of bins:",
+                   min = 1,
+                   max = 50,
+                   value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
