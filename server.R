dfc87c1415971e36160eb8f9b480ed932a8fc86f
jinc132 <jinc132@u.washington.edu>
2019-03-07 16:56:27 -0800

    Add files

--- server.R
+++ server.R
@@ -1,7 +1,13 @@
 library(shiny)
+library(plotly)
+library(dplyr)
 
 # supress warning messages
 suppressWarnings(warnings)
+
+#include other necessary files
+source("FreqMap.R")
+source("staticMap.R")
 
 shinyServer(function(input, output) {
   
