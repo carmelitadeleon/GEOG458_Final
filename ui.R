6938a048d2b82fbe5ba0c65f6b66aa78206be531
jinc132 <jinc132@u.washington.edu>
2019-03-12 00:58:39 -0700

    add text

--- ui.R
+++ ui.R
@@ -15,22 +15,34 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       conditionalPanel(
-        condition = "input.tabs == 'Welcome!'"
-      ),
-      conditionalPanel(
-        condition = "input.tabs == 'Export Values'"
+        condition = "input.tabs == 'Export Values'",
+        
+        h3("Overview"),
+        tags$div(class="about",
+                 tags$p(evOverview)
+        )
       ),
       conditionalPanel(
         condition = "input.tabs == 'Export Type'",
         selectInput("exportType",
                     h3("Please select the export type: "),
                     choices = exportTp),
+        
+        h3("Overview"),
+        tags$div(class="about",
+                 tags$p(prodOverview)
+        ),
+        
+        h3("Search Export Product"),
         textOutput("hsCode"),
-        HTML("<br><p>For more information regarding this export please look up the hs code 
-             <a href='https://www.foreign-trade.com/reference/hscode.htm'>here</a></p>")
+        tags$div(class="lookup",
+                 tags$p(HTML("<br>"), "For more information regarding this export please look up the HS code",
+                        tags$a(href='https://www.foreign-trade.com/reference/hscode.htm', "here")
+                 )
+        )
       ),
       conditionalPanel(
-        condition = "input.tabs == 'INSERT2'"
+        condition = "input.tabs == 'LQ'"
       ),
       conditionalPanel(
         condition = "input.tabs == 'Bibliography'"
@@ -42,8 +54,8 @@ shinyUI(fluidPage(
       tabsetPanel(id = "tabs",
                   tabPanel("Welcome!"),
                   tabPanel("Export Values",
-                           HTML("<p align= 'left' style='padding: 1em 7em 0em 10em'>
-                      <font size= '5'>United States Export Frequency Values</font></p>"),
+                           HTML("<p align= 'left' style='padding: 1em 7em 0em 15em'>
+                      <font size= '5'>United States Export Values</font></p>"),
                            leafletOutput("freqMap", width="720px",height="680px")
                   ),
                   tabPanel("Export Type",
